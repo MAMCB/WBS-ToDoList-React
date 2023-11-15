e0114494415bfb3663809e9fa9105631ae2ccb30
MAMCB <96302464+MAMCB@users.noreply.github.com>
2023-11-15 18:07:34 +0100

    created first component Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Form from "./Components/Form"
 import './App.css'
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <h1>To do List</h1>
+      <Form/>
     </>
   )
 }
---
+++ src/Components/Form.jsx
@@ -0,0 +1,15 @@
+const Form = ()=>{
+
+
+    return(
+        <>
+        <form action="">
+            <input type="text" placeholder="Add task" />
+            <button>+</button>
+
+            
+        </form>
+        </>
+    )
+}
+export default Form;
