f512c19dcecfadb3072ab6f7361944bc0a66ccc3
MAMCB <96302464+MAMCB@users.noreply.github.com>
2023-11-17 12:06:43 +0100

    added tabs module and refactored displayTasks

--- src/Components/DisplayTasks.jsx
+++ src/Components/DisplayTasks.jsx
@@ -1,4 +1,6 @@
 import Task from "./Task";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const DisplayTasks = ({tasks,setTasks,updateTaskArray,deleteTaskFromArray})=>{
     const toggleCompletion = (id) =>{
@@ -7,11 +9,36 @@ const DisplayTasks = ({tasks,setTasks,updateTaskArray,deleteTaskFromArray})=>{
     }
     return (
         <>
-        <ul className="list-group">
-            {tasks.length>0?tasks.sort((a,b)=>(a.done?1:-1)).map((task)=>(<Task key={task.id} task={task} toggleCompletion={toggleCompletion} tasks={tasks} updateTaskArray={updateTaskArray} deleteTaskFromArray={deleteTaskFromArray}/>)):<p>No tasks</p>}
+        {tasks.length>0? <Tabs>
+                            <TabList>
+                                <Tab>All tasks</Tab>
+                                <Tab>High priority</Tab>
+                                <Tab>Medium priority</Tab>
+                                <Tab>Low priority</Tab>
+                            </TabList>
+
+                            <TabPanel>
+                                <h2>All tasks</h2>
+                                <ul className="list-group">
+            {tasks.sort((a,b)=>(a.done?1:-1)).map((task)=>(<Task key={task.id} task={task} toggleCompletion={toggleCompletion} tasks={tasks} updateTaskArray={updateTaskArray} deleteTaskFromArray={deleteTaskFromArray}/>))}
            
             
-      </ul>
+                                </ul>
+                            </TabPanel>
+                            <TabPanel>
+                                <h2>High priority tasks</h2>
+                            </TabPanel>
+                            <TabPanel>
+                                <h2>Medium priority tasks</h2>
+                            </TabPanel>
+                            <TabPanel>
+                                <h2>Low priority tasks</h2>
+                            </TabPanel>
+                        </Tabs>
+        
+
+
+        :<p>No tasks</p>}
         </>
     )
 }
