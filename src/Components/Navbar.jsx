361926171c997ca70690d6f972ef64272cc913f7
MAMCB <96302464+MAMCB@users.noreply.github.com>
2023-11-20 14:25:07 +0100

    added navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = ()=>{
+return (
+    <>
+    <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <img src="" alt="logo" />
+    <a class="navbar-brand">To do list</a>
+    
+  </div>
+</nav>
+
+
+    </>
+)
+};
+export default Navbar
